2d15ffa69182bc4af4b0e584414f4d664766a981
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2025-08-05 15:09:45 +0200

    Finished implementing the marketing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return(
     <div>
-      <Button>Click me</Button>
+      <p>This is a marketing page</p>
     </div>
   )
 }
