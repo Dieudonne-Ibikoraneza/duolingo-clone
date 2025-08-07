22ad8c4ad378bdc84ec963ccc20b952f624c8e55
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2025-08-07 15:09:46 +0200

    feat: finished implementing the learn page wrappers

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import React from "react";
+import { Header } from "./header";
+import { UserProgress } from "@/components/user-progress";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title="Spanish" />
+      </FeedWrapper>
+    </div>
+  );
+};
 
-export default page
+export default page;
