69f1f866d34413378cdc5ec5b33584497c72ac4a
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2025-08-07 22:04:00 +0200

    feat: finished the user progress implementation

--- db/queries.ts
+++ db/queries.ts
@@ -1,11 +1,34 @@
 import { cache } from "react";
 import db from "@/db/drizzle";
-import { asc } from "drizzle-orm";
-import { courses } from "@/db/schema";
+import { asc, eq } from "drizzle-orm";
+import { courses, userProgress } from "@/db/schema";
+import { auth } from "@clerk/nextjs/server";
+
+export const getUserProgress = cache(async () => {
+  const { userId } = await auth();
+
+  if (!userId) return null;
+
+  const data = await db.query.userProgress.findFirst({
+    where: eq(userProgress.userId, userId),
+    with: {
+      activeCourse: true,
+    },
+  });
+  return data;
+});
 
 export const getCourses = cache(async () => {
   const data = await db.query.courses.findMany({
     orderBy: [asc(courses.id)],
+  });
+
+  return data;
+});
+
+export const getCourseById = cache(async (courseId: number) => {
+  const data = await db.query.courses.findFirst({
+    where: eq(courses.id, courseId),
   });
 
   return data;
