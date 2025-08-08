f3e247ed0eead717f77865c131176d78966dc6b7
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2025-08-08 23:49:24 +0200

    feat: implemented the lesson header component

---
+++ app/lesson/header.tsx
@@ -0,0 +1,40 @@
+import { Progress } from "@/components/ui/progress";
+import { InfinityIcon, X } from "lucide-react";
+import Image from "next/image";
+
+type Props = {
+  hearts: number;
+  percentage: number;
+  hasActiveSubscription: boolean;
+};
+
+export const Header = ({
+  hearts,
+  percentage,
+  hasActiveSubscription,
+}: Props) => {
+  return (
+    <header className="lg:pt-[50px] pt-[20px] px-10 flex gap-x-7 items-center justify-between max-w-[1140px] mx-auto w-full">
+      <X
+        onClick={() => {}}
+        className="text-slate-500 hover:opacity-75 transition cursor-pointer"
+      />
+      <Progress value={percentage} />
+      <div className="text-rose-500 flex items-center font-bold">
+        <Image
+          src="/heart.svg"
+          alt="Heart"
+          className="mr-2"
+          width={28}
+          height={28}
+        />
+
+        {hasActiveSubscription ? (
+          <InfinityIcon className="stroke-[3] w-6 h-6" />
+        ) : (
+          hearts
+        )}
+      </div>
+    </header>
+  );
+};
